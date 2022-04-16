f4294f7d7416ae473dc9b5fa3d3c9f49c38158ca
Roscoking <ohc4@alu.ua.es>
2022-04-16 15:45:52 +0200

    Register password generation implemented

--- Cliente/src/application/registerController.java
+++ Cliente/src/application/registerController.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import main.Main;
 
 public class registerController {
 	
@@ -22,12 +24,63 @@ public class registerController {
 	@FXML
 	private TextField confirmPasswordTextField;
 	
+	@FXML
+	private Label errorLabel;
+	
 	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
+	public Boolean nullFields(){
+		
+		boolean result = true;
+		
+		if(nicknameTextField.getText() == null || passwordTextField.getText() == null || confirmPasswordTextField.getText() == null) {
+			result = false;
+		}
+		
+		return result;
+	}
+	
+	public Boolean confirmPassword() {
+		boolean result = true;
+		
+		if(passwordTextField.getText() != confirmPasswordTextField.getText()) {
+			result = false;
+		}
+		
+		return result;
+	}
+	
 	public void register(ActionEvent e) {
+		
+		if(nullFields()) {
+			if(confirmPassword()) {
+				
+				String nick = nicknameTextField.getText();
+				String password = passwordTextField.getText();
+				
+				//Conexion a server
+			}
+			else {
+				errorLabel.setText("Passwords must be equal");
+			}
+		}
+		else {
+			errorLabel.setText("All fields must be completed");
+		}
+	}
+	
+	public void generatePassword(ActionEvent e) {
+		
+		Main passwGen = new Main();
+		
+		String securePass = passwGen.generatePassword(64, true, true, true, true);
+		
+		passwordTextField.setText(securePass);
+		
+		confirmPasswordTextField.setText(securePass);
 		
 	}
 
